439f6a27dc7b3380a94f8af245ded7b7053a7175
soundarya-dvlp <155609480+soundarya-dvlp@users.noreply.github.com>
2024-08-09 10:01:37 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,9 @@
+document.getElementById("demo1").style.color="white"
+document.getElementById("demo2").style.color="yellow"
+document.getElementById("demo3").style.color="red"
+document.getElementById("demo4").style.color="red"
+document.getElementById("demo5").style.color="red"
+
+
+
+
